9fa169ecdbadd2e85b408a795a9235e9075fb1cb
ABDURAHIMAN K <laluvga@gmail.com>
2025-03-01 10:00:29 +0530

    added with and without pareto

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Benders-Decomposition-with-Pareto-Optimal-Cut
 An example of Benders Decomposition with Pareto Optimal Cut using CPLEX library with C++.
+
+### Problems
+1. I have encountered one problem. That is for this mathematical model, even without using pareto oprtimal cut, <br>
+we will get optimal solution.
--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+// An Example for Benders Decomposition
 /************************************************************************************
  * This example on Benders Decomposition is taken from the tutorial on Benders Decomposition.
  * Refer to Benders-Slides.pdf
@@ -7,18 +8,21 @@
  * X1 + X2 + 4X3 + Y >= 10
  * X1, X2, X3 >= 0, Y integer in {2,12}
  ***********************************************************************************/
-#pragma warning(disable : 4996)//For Visual Studio 2012
-#include<stdio.h>
-#include<conio.h>
-#include<iostream>
-#include<fstream>
-#include<iosfwd>
-#include<string>
+
+ //////WITHOUT PARETO OPTIMAL CUT//////
+
+#pragma warning(disable : 4996) //For Visual Studio 2012
+#include <stdio.h>
+#include <conio.h>
+#include <iostream>
+#include <fstream>
+#include <iosfwd>
+#include <string>
 #include <deque>
 #include <sstream>
 #include <time.h>
 #include <stdlib.h>
-#include <vector>//for vectors
+#include <vector> //for vectors
 #include <math.h>
 
 #include <ilcplex/ilocplex.h>
@@ -37,14 +41,12 @@ int main(int argc, char** argv)
 		IloNumVarArray X_dual(env, 2, 0, IloInfinity, ILOFLOAT);
 		IloNum Y_val;
 		IloNum theta_val;
-		IloNumVarArray aux_dual(env, 2, 0, IloInfinity, ILOFLOAT); // new
 
 		////////DECISION VARIABLES AND PARAMETERS FOR MASTER PROBLEM///////////
 		IloNumVar Y(env, 2, 12, ILOINT);
 		IloNumArray X_dual_val(env, 2);
 		//IloNumVar theta_var(env, -IloInfinity, IloInfinity, ILOFLOAT);
 		IloNumVar theta_var(env, 0, IloInfinity, ILOFLOAT);
-		IloNumArray D_aux_val(env, 2, 0, IloInfinity); // new
 
 		//////////DEVELOP GENERIC MODEL //////////////////////////
 
@@ -74,26 +76,6 @@ int main(int argc, char** argv)
 		cplex_sub.setOut(env.getNullStream()); // This is to supress the output of Branch & Bound Tree on screen
 		cplex_sub.setWarning(env.getNullStream()); //This is to supress warning messages on screen
 
-
-		/////////Auxiliary Subproblem (For Pareto-Optimal Cut)//////////////////////
-		IloModel model_aux_sub(env);
-		IloExpr obj_aux_sub(env);
-
-		// Y should be in the relative interior (2 < Y < 12), then we will have only one solution
-		// choosing y as 5 (obj fun of sub problem = max (12-y)u1 + (10-y)u2
-		obj_aux_sub = 7 * aux_dual[0] + 5 * aux_dual[1];
-		model_aux_sub.add(IloMaximize(env, obj_aux_sub));
-		model_aux_sub.add(3 * aux_dual[0] + aux_dual[1] <= 18);
-		model_aux_sub.add(aux_dual[0] + aux_dual[1] <= 8);
-		model_aux_sub.add(aux_dual[0] + 4 * aux_dual[1] <= 20);
-
-		IloCplex cplex_aux_sub(env);
-		obj_aux_sub.end();
-		cplex_aux_sub.setOut(env.getNullStream());
-		cplex_aux_sub.setWarning(env.getNullStream());
-
-
-
 		/////////BEGIN ITERATIONS/////////////////////////////////
 		IloNum GAP = IloInfinity;
 		theta_val = 0;
@@ -122,47 +104,17 @@ int main(int argc, char** argv)
 			cout << "Sub Problem Solution Status: " << cplex_sub.getCplexStatus() << endl;
 			if (cplex_sub.getCplexStatus() == CPX_STAT_OPTIMAL)
 			{// Dual subproblem is bounded; Add Optimality Cut to the Master Problem
-				cplex_sub.getValues(X_dual_val, X_dual);
+				cplex_sub.getValues(X_dual_val, X_dual);  // taking values of X_dual from SP and saves to X_dual_val
 				cout << "X_dual = " << X_dual_val << endl;
 				sub_obj_val = cplex_sub.getObjValue();
 				cout << "sub_obj_val = " << sub_obj_val << endl;
 				Upper_bound = IloMin(Upper_bound, (10 * Y_val + sub_obj_val));
 				cout << "Upper_bound = " << Upper_bound << endl;
 
-
-
-				cplex_aux_sub.setParam(IloCplex::PreInd, IloFalse);  // Disable presolve
-				cplex_aux_sub.setParam(IloCplex::RootAlg, IloCplex::Barrier);  // Try a different solver
-
-
-				///// Solve Auxiliary Subproblem for Pareto-Optimal Cut
-				cplex_aux_sub.extract(model_aux_sub);
-				IloRange r1(env, sub_obj_val, (12 - 2 * Y_val) * aux_dual[0] + (10 - Y_val) * aux_dual[1], sub_obj_val);
-				IloExpr aux_obj(env);
-
-				model_aux_sub.add(r1);
-				cplex_aux_sub.solve();
-				cplex_aux_sub.getValues(D_aux_val, aux_dual);
-
-
-				cout << "SOLVING AUXILIARY SUBPROBLEM" << endl;
-				cplex_aux_sub.extract(model_aux_sub);
-				cplex_aux_sub.solve();
-				cout << "Auxiliary Subproblem Solution Status: " << cplex_aux_sub.getCplexStatus() << endl;
-
-				IloNum cut_coeff = (2 * D_aux_val[0] + D_aux_val[1]);
-				IloNum cut_rhs = (12 * D_aux_val[0] + 10 * D_aux_val[1]);
-				cout << "Pareto-Optimal Cut Added: theta + " << cut_coeff << " * Y >= " << cut_rhs << endl;
-				model_aux_sub.remove(r1);
-				model_master.add(theta_var + cut_coeff * Y >= cut_rhs);
-
-
-
-
 				//Add Cut to the Master Problem
-				//cout << "Cut Added to Master Problem: " << "theta + " << (2 * X_dual_val[0] + X_dual_val[1]) << " Y >= " << 6 * X_dual_val[0] + 10 * X_dual_val[1] << endl;
-				//model_master.add(theta_var + (2 * X_dual_val[0] + X_dual_val[1]) * Y >=
-					//6 * X_dual_val[0] + 10 * X_dual_val[1]);
+				cout << "Cut Added to Master Problem: " << "theta + " << (2 * X_dual_val[0] + X_dual_val[1]) << " Y >= " << 12 * X_dual_val[0] + 10 * X_dual_val[1] << endl;
+				model_master.add(theta_var + (2 * X_dual_val[0] + X_dual_val[1]) * Y >=
+					12 * X_dual_val[0] + 10 * X_dual_val[1]);
 			}
 			cout << "SOLVING MASTER PROBLEM" << endl;
 			cout << "Master Problem Solution Status: " << cplex_master.getCplexStatus() << endl;
